02ba31819091fe20b4b3ab60e0adb5b3a93d4ec8
Keana Patman <missk913@gmail.com>
2026-09-25 14:21:41 -0600

    adding products

--- pages/views.py
+++ pages/views.py
@@ -17,3 +17,14 @@ class AboutPageView(TemplateView):
         context["phone_number"] = "555-555-5555"
         return context
 
+class ProductsPageView(TemplateView):
+    template_name = "products.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product1"] = "Industrial Sized Rubber Ducks"
+        context["product2"] = "USB that Grants Access to The Orb"
+        context["product3"] = "15 Cheeseburgers, Hold the Cheese"
+        context["product4"] = "Half of a Single Sock"
+        return context
+
